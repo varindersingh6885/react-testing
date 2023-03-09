b2552e94f38a314c98a00dd30e5662e00c827642
Varinder Singh <varinder.singh01@nagarro.com>
2023-03-09 15:29:36 +0530

    getByRole Options

--- src/components/applicationForm/applicationForm.test.jsx
+++ src/components/applicationForm/applicationForm.test.jsx
@@ -5,12 +5,53 @@ import ApplicationForm from "./applicationForm";
 // Various default roles for HTML elements
 // https://www.w3.org/TR/html-aria/#docconformance
 
+// Options for getByRole method
+// https://testing-library.com/docs/queries/byrole/#api
+
+/*
+Options :
+
+    getByRole(
+        role: string,
+        options?: {
+            hidden?: boolean = false,
+            name?: TextMatch,  // https://www.tpgi.com/what-is-an-accessible-name/
+            description?: TextMatch,
+            selected?: boolean,
+            checked?: boolean,
+            pressed?: boolean,
+            suggest?: boolean,
+            current?: boolean | string,
+            expanded?: boolean,
+            queryFallbacks?: boolean,
+            level?: number,
+        }
+    ): HTMLElement
+*/
+
 describe("Application Form Component", () => {
     test("Form renders successfully", () => {
         render(<ApplicationForm />)
 
-        const nameElement = screen.getByRole('textbox');
+        const h1Element = screen.getByRole('heading', {
+            level: 1
+        });
+        expect(h1Element).toBeInTheDocument();
+
+        const h2Element = screen.getByRole("heading", {
+            level: 2
+        })
+        expect(h2Element).toBeInTheDocument()
+
+        const nameElement = screen.getByRole('textbox', {
+            name: 'Name'
+        });
         expect(nameElement).toBeInTheDocument();
+
+        const bioElement = screen.getByRole('textbox', {
+            name: "Bio"
+        })
+        expect(bioElement).toBeInTheDocument();
 
         const jobLocationElement = screen.getByRole("combobox");
         expect(jobLocationElement).toBeInTheDocument();
